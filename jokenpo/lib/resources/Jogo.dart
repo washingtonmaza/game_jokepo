727d0d6cb99c8d4e4f6e39156766cb97c522b057
Washington Maza <washingtonnegocios@bol.com.br>
2021-03-22 19:17:17 -0300

    First commit

--- jokenpo/lib/resources/Jogo.dart
+++ jokenpo/lib/resources/Jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -6,12 +8,65 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+  var _imagemApp = AssetImage('lib/resources/images/select.png');
+  var _mensagem = 'Escolha opção abaixo';
+
+  void checkWin(String escolhaUsuario) {
+    var opcoes = ['pedra', 'papel', 'tesoura'];
+    int randomNumber = Random().nextInt(3);
+    var choseMachine = opcoes[randomNumber];
+
+    switch (choseMachine) {
+      case "pedra":
+        setState(() {
+          this._imagemApp = AssetImage('lib/resources/images/pedra.png');
+        });
+
+        break;
+      case "papel":
+        setState(() {
+          this._imagemApp = AssetImage('lib/resources/images/papel.png');
+        });
+
+        break;
+      case "tesoura":
+        setState(() {
+          this._imagemApp = AssetImage('lib/resources/images/tesoura.png');
+        });
+
+        break;
+      default:
+        setState(() {
+          this._imagemApp = AssetImage('lib/resources/images/select.png');
+        });
+    }
+    if ((escolhaUsuario == 'pedra' && choseMachine == 'tesoura') ||
+        (escolhaUsuario == 'papel' && choseMachine == 'pedra') ||
+        (escolhaUsuario == 'tesoura' && choseMachine == 'papel')) {
+      print('gasd');
+      this.setState(() {
+        _mensagem = 'Você ganhou';
+      });
+    } else if ((choseMachine == 'pedra' && escolhaUsuario == 'tesoura') ||
+        (choseMachine == 'papel' && escolhaUsuario == 'pedra') ||
+        (choseMachine == 'tesoura' && escolhaUsuario == 'papel')) {
+      print('perdeu');
+      this.setState(() {
+        _mensagem = 'Você perdeu';
+      });
+    } else {
+      this.setState(() {
+        _mensagem = 'Empate';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            "JokenPO",
+            'JokenPO',
           ),
         ),
         body: Column(
@@ -21,21 +76,48 @@ class _JogoState extends State<Jogo> {
               'Escolha do APP',
               style: TextStyle(fontSize: 20),
             ),
-            Image.asset(
-              'lib/resources/images/select.png',
-              width: 150,
-              height: 150,
+            GestureDetector(
+              onTap: () {},
+              child: Image(
+                image: this._imagemApp,
+                width: 150,
+                height: 150,
+              ),
             ),
-            Text('voce perdeu'),
+            Text(
+              _mensagem,
+              style: TextStyle(color: Colors.blue, fontSize: 18),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Image.asset('lib/resources/images/pedra.png',
-                    width: 100, height: 100),
-                Image.asset('lib/resources/images/papel.png',
-                    width: 100, height: 100),
-                Image.asset('lib/resources/images/tesoura.png',
-                    width: 100, height: 100),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      checkWin('pedra');
+                    });
+                  },
+                  child: Image.asset('lib/resources/images/pedra.png',
+                      width: 100, height: 100),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      checkWin('papel');
+                    });
+                  },
+                  child: Image.asset('lib/resources/images/papel.png',
+                      width: 100, height: 100),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      checkWin('tesoura');
+                    });
+                  },
+                  child: Image.asset('lib/resources/images/tesoura.png',
+                      width: 100, height: 100),
+                ),
               ],
             ),
           ],
---
+++ jokenpo/lib/resources/controller/check_win.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CheckWin extends StatefulWidget {
+  @override
+  _CheckWinState createState() => _CheckWinState();
+}
+
+class _CheckWinState extends State<CheckWin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
